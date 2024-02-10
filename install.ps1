5b8f6fc3c6db2ed2b815d8e672e2e29640de5ad3
Tom Plant <tom@tplant.com.au>
2024-02-10 21:20:59 +1100

    feat: install WSL and configure PowerShell

--- install.ps1
+++ install.ps1
@@ -3,11 +3,12 @@
 # Windows Updates
 # Upgrade SKU if needed
 # Update store apps
+# Create 32GB Dev Drive
 
 [CmdletBinding(SupportsShouldProcess)]
 param()
 
-start custom.deskthemepack
+wsl --install --no-distribution
 
 # winget export packages.json
 winget import winget.json --accept-source-agreements --accept-package-agreements
@@ -15,8 +16,19 @@ if ($PSCmdlet.ShouldContinue("personal packages?", "winget import")) {
   winget import winget-personal.json --accept-source-agreements --accept-package-agreements
 }
 
+mkdir D:\repos
+"PowerShell", "WindowsPowerShell" | % {
+  $packages = Join-Path ([Environment]::GetFolderPath("MyDocuments")) $_
+  mkdir D:\packages\$_
+  New-Item -ItemType Junction $packages -Target D:\packages\$_
+  New-Item -ItemType HardLink (Join-Path $packages Microsoft.PowerShell_profile.ps1) -Target Microsoft.PowerShell_profile.ps1
+}
+Set-PackageSource PSGallery -Trusted
+pwsh -Command {
+  Set-PackageSource PSGallery -Trusted
+}
+
 oh-my-posh font install --user FiraCode
-New-Item -ItemType HardLink $PROFILE -Target Microsoft.PowerShell_profile.ps1
 
 New-Item -ItemType HardLink $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Target terminal.json
 
@@ -24,9 +36,11 @@ Get-Item *.gitconfig | ForEach-Object {
   New-Item -ItemType HardLink (Join-Path $HOME $_.Name) -Target $_.Name
 }
 
+start custom.deskthemepack
+
 # Extend monitor
 # Set language
-# Set lcok screen
+# Set lock screen
 # Taskbar - Explorer, Terminal, Edge, VSCode
 # Unpin/uninstall start menu
 # Alt-tab don't show app tabs
@@ -35,3 +49,4 @@ Get-Item *.gitconfig | ForEach-Object {
 # Explorer show file extensions and hidden files
 # Sign in to Bitwarden and GitHub
 # VSCode sync settings with GitHub
+# Edge search engine and sidebar
