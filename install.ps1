779f7bf1479eb4583b88e2d0449014aa42aa672d
Tom Plant <tom@tplant.com.au>
2024-02-10 19:16:01 +1100

    fix: Windows symlinks, winget args

--- install.ps1
+++ install.ps1
@@ -4,16 +4,16 @@ param()
 start custom.deskthemepack
 
 # winget export packages.json
-winget import winget.json
-if($PSCmdlet.ShouldProcess("personal packages", "winget import")){
-  winget import winget-personal.json
+winget import winget.json --accept-source-agreements --accept-package-agreements
+if ($PSCmdlet.ShouldContinue("personal packages?", "winget import")) {
+  winget import winget-personal.json --accept-source-agreements --accept-package-agreements
 }
 
 oh-my-posh font install --user FiraCode
-New-Item -ItemType SymbolicLink $PROFILE -Target Microsoft.PowerShell_profile.ps1
+New-Item -ItemType HardLink $PROFILE -Target Microsoft.PowerShell_profile.ps1
 
-New-Item -ItemType SymbolicLink $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Target terminal.json
+New-Item -ItemType HardLink $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Target terminal.json
 
 Get-Item *.gitconfig | ForEach-Object {
-  New-Item -ItemType SymbolicLink (Join-Path $HOME $_.Name) -Target $_.Name
+  New-Item -ItemType HardLink (Join-Path $HOME $_.Name) -Target $_.Name
 }
