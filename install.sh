4f9f6494ba53ea35188ec579dd26d2c1ad8c5d65
Tom Plant <tom@tplant.com.au>
2024-11-23 13:28:56 +1100

    fix: remove unsupported oh-my-posh arg

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/bin
 export PATH="$PATH:$HOME/bin"
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/bin
-oh-my-posh font install --user FiraCode
+oh-my-posh font install FiraCode
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
