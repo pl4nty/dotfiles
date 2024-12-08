332c819772a1601693766b117f769ad4ba900bce
Tom Plant <tom@tplant.com.au>
2024-12-08 13:47:11 +1100

    fix: oh-my-posh ignore expired certificate
    https://github.com/JanDeDobbeleer/oh-my-posh/issues/5986

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/bin
 export PATH="$PATH:$HOME/bin"
-curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/bin
+curl -s --insecure https://ohmyposh.dev/install.sh | bash -s -- -d ~/bin
 oh-my-posh font install FiraCode
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
