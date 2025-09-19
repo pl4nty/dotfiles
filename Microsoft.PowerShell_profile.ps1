9da95212bfa045b1f7d7ccbfb3034ae26fc23b1f
Tom Plant <tom@tplant.com.au>
2025-09-19 17:28:27 +1000

    chore: sync from work
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,9 @@
+# workaround for broken UTF-8 support in WDAC audit mode
 [Console]::OutputEncoding = [Text.Encoding]::UTF8
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/lightgreen.omp.json" | Invoke-Expression
+
+oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/lightgreen.omp.json | Invoke-Expression
 
 Import-Module PSReadLine
-
-function ?? {
-  gh copilot suggest --target shell ('use powershell to ' + $args)
-}
 
 $null = Register-EngineEvent -SourceIdentifier 'PowerShell.OnIdle' -MaxTriggerCount 1 -Action {
   Import-Module -Name UsefulArgumentCompleters -Global
@@ -22,43 +20,4 @@ $null = Register-EngineEvent -SourceIdentifier 'PowerShell.OnIdle' -MaxTriggerCo
   }
 }
 
-function Add-AutopilotToVM {
-  [CmdletBinding()]
-  param(
-    [parameter(ValueFromPipeline,Mandatory)][Microsoft.HyperV.PowerShell.VirtualMachine]$VM,
-    [parameter(Mandatory)][string]$Tenant
-  )
-  begin {
-    $oidc = Invoke-RestMethod https://login.microsoftonline.com/$Tenant/.well-known/openid-configuration
-    $file = New-TemporaryFile
-    @{
-      # Version=2049
-      CloudAssignedTenantId=$oidc.authorization_endpoint -replace "https://login.microsoftonline.com/([^/]+).+", '$1'
-      CloudAssignedTenantDomain=$Tenant
-      CloudAssignedOobeConfig=28
-      CloudAssignedDomainJoinMethod=0
-      CloudAssignedForcedEnrollment=1
-      ZtdCorrelationId=(New-Guid).Guid
-      CloudAssignedAadServerData="{`"ZeroTouchConfig`":{`"CloudAssignedTenantUpn`":`"`",`"CloudAssignedTenantDomain`":`"$Tenant`"}}"
-      # CloudAssignedDeviceName
-      Comment_File="Manual Profile"
-    } | ConvertTo-Json | Set-Content $file
-  }
-  process {
-    $_ | Enable-VMIntegrationService -Name "Guest Service Interface" # Copy-VMFile dependency
-    $_ | Enable-VMTPM -PassThru | Copy-VMFile -FileSource Host -Force -SourcePath $file -DestinationPath "C:\Windows\Provisioning\Autopilot\AutopilotConfigurationFile.json"
-  }
-  end {
-    Remove-Item $file
-  }
-}
-
-function Tail ($Log = "Application", $Pastmins = 5) {
-  $lastdate = $(Get-Date).AddMinutes(-$Pastmins)
-  while ($True) {
-    $newdate = Get-Date
-    Get-WinEvent $Log -ea 0 | where TimeCreated -ge $lastdate -and TimeCreated -le $newdate | Sort-Object TimeCreated
-    $lastdate = $newdate
-    Start-Sleep -Milliseconds 330
-  }
-}
+komac autocomplete powershell | out-string | iex
